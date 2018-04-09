a8a64d9873ca22822ecd8b7f5bf5387be1d2d7a3
rmc-coma <rmc-coma@student.42.fr>
2018-04-10 01:25:54 +0200

    fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { DataPage } from '../data/data';
 
 @Component({
   selector: 'page-home',
@@ -32,7 +33,7 @@ export class HomePage {
     if (this.loggedIn = true) {
       this.navCtrl.push(LoginPage);
     } else {
-      this.navCtrl.push(DataPage);
+      this.navCtrl.push(DataPage, {LoggedIn: this.loggedIn});
     }
   }
 
